52b6f1a5aff7fc30b7776394a6898b6adcfd8c23
Artem Khimin <a.khimin@yandex.ru>
2023-11-05 12:24:05 +0300

    checking cards

--- main.py
+++ main.py
@@ -1,8 +1,136 @@
+import time
 
 import game_card
+import random_card
+
+play1 = []
+comp = []
+answer = str
 
 print('Добро пожаловать в игру ЛОТО')
+name = input('Как к Вам обращаться: ')
+input('Выберите карточку, нажав любую клавишу!')
 
-play1=game_card.Game_Card
 
-play1.card_selection()
+# play1=game_card.Game_Card
+# play1.card_selection()
+
+
+def pr_card(card):
+    """
+    Печать карточки
+    :param card:
+    :return:
+    """
+    print('*' * 40)
+    print(card[0])
+    print(card[1])
+    print(card[2])
+    print('*' * 40)
+    print()
+
+
+def field():
+    """
+    Печать поля
+    :return:
+    """
+    print('Карточка компьютера: ')
+    pr_card(comp)
+    print()
+    print(f'Карточка игрока {name}: ')
+    pr_card(play1)
+    print()
+
+
+def steep():
+    """
+    Шаг игры
+    :return:
+    """
+    print()
+    print('Происходит перемешивание боченков.....')
+    time.sleep(1)
+    print(3)
+    time.sleep(1)
+    print(2)
+    time.sleep(1)
+    print(1)
+    time.sleep(1)
+    print('Боченки перемешаны. Начинаем игру!')
+    time.sleep(1)
+
+
+# Боченки текущей игры
+barrels_game = random_card.barrels_mix()
+
+
+def check_card(play, barrel):
+    pass
+
+
+def checking_cards(play1, comp, barrel):
+    """
+    Проверка карточек
+    :param play1:
+    :param comp:
+    :param barrel:
+    :return:
+    """
+    for line in play1:
+        for brl in line:
+            if brl == barrel:
+                play1[line[brl]] = 'XX'
+                if answer == '0':
+                    print(f'Вы проиграли, так как у Вас есть боченок №{barrel}')
+                    break
+    if answer == '1':
+        print(f'Вы проиграли, так как у Вас нет боченка №{barrel}, а Вы отметили, что есть!')
+
+
+# print(barrels_game)
+def game():
+    """
+    Игра
+    :return:
+    """
+    counter = 0
+    while True:
+        steep()
+
+        for barrel in barrels_game:
+            field()
+            print(f'Вытащен боченок №{barrel}:')
+            print()
+            while True:
+                answer = input('Если у Вас есть данный боченок нажмите "1", если нет, то "0" ')
+                if answer == '1':
+                    print('1')
+                    break
+                if answer == '0':
+                    print('0')
+                    break
+
+            checking_cards(play1, comp, barrel)
+            print('Прошёл проверку')
+
+
+while True:
+    play1 = random_card.new_card()
+    pr_card(play1)
+    select = input('Нажмите "1" для выбора данной карточки или любую клавишу для продолжения выбора: ')
+
+    if select == '1':
+        break
+
+    print()
+    print()
+
+# print('333')
+# print(play1)
+
+print('Компьютер выбирает такую карточку: ')
+comp = random_card.new_card()
+pr_card(comp)
+
+game()
--- random_card.py
+++ random_card.py
@@ -1,130 +1,182 @@
 import random
 
-# card=random.randint(100)
-card_mix = []
 
-barrels = [i for i in range(91)]
-random.shuffle(barrels)
-
-print(barrels)
-# line1 = barrels[:10]
-# line2 = barrels[10:20]
-# line3 = barrels[20:30]
-#
-# print(line1)
-# print(line2)
-# print(line3)
-numbers1_10 = []
-numbers11_20 = []
-numbers21_30 = []
-numbers31_40 = []
-numbers41_50 = []
-numbers51_60 = []
-numbers61_70 = []
-numbers71_80 = []
-numbers81_90 = []
-
-new_barrels=[]
-
-for i in barrels:
-    # print(barrels[i])
-    if 1 < barrels[i]<= 10:
-        numbers1_10.append(barrels[i])
-new_barrels.append(numbers1_10)
-
-for i in barrels:
-    # print(barrels[i])
-    if 10 < barrels[i]<= 20:
-        numbers11_20.append(barrels[i])
-new_barrels.append(numbers11_20)
+def barrels_mix():
+    barrels = [i for i in range(91)]
+    random.shuffle(barrels)
+    return barrels
 
 
-for i in barrels:
-    # print(barrels[i])
-    if 20 < barrels[i]<= 30:
-        numbers21_30.append(barrels[i])
-new_barrels.append(numbers21_30)
+def new_card():
+    # card=random.randint(100)
+    card_mix = []
+    barrels = barrels_mix()
 
-for i in barrels:
-    # print(barrels[i])
-    if 30 < barrels[i]<= 40:
-        numbers31_40.append(barrels[i])
-new_barrels.append(numbers31_40)
+    # print(barrels)
+    # line1 = barrels[:10]
+    # line2 = barrels[10:20]
+    # line3 = barrels[20:30]
+    #
+    # print(line1)
+    # print(line2)
+    # print(line3)
+    numbers1_10 = []
+    numbers11_20 = []
+    numbers21_30 = []
+    numbers31_40 = []
+    numbers41_50 = []
+    numbers51_60 = []
+    numbers61_70 = []
+    numbers71_80 = []
+    numbers81_90 = []
 
-for i in barrels:
-    # print(barrels[i])
-    if 40 < barrels[i]<= 50:
-        numbers41_50.append(barrels[i])
-new_barrels.append(numbers41_50)
+    new_barrels = []
 
-for i in barrels:
-    # print(barrels[i])
-    if 50 < barrels[i]<= 60:
-        numbers51_60.append(barrels[i])
-new_barrels.append(numbers51_60)
+    for i in barrels:
+        # print(barrels[i])
+        if 1 < barrels[i] <= 10:
+            numbers1_10.append(barrels[i])
+    new_barrels.append(numbers1_10)
 
-for i in barrels:
-    # print(barrels[i])
-    if 60 < barrels[i]<= 70:
-        numbers61_70.append(barrels[i])
-new_barrels.append(numbers61_70)
+    for i in barrels:
+        # print(barrels[i])
+        if 10 < barrels[i] <= 20:
+            numbers11_20.append(barrels[i])
+    new_barrels.append(numbers11_20)
 
-for i in barrels:
-    # print(barrels[i])
-    if 70 < barrels[i]<= 80:
-        numbers71_80.append(barrels[i])
-new_barrels.append(numbers71_80)
+    for i in barrels:
+        # print(barrels[i])
+        if 20 < barrels[i] <= 30:
+            numbers21_30.append(barrels[i])
+    new_barrels.append(numbers21_30)
 
-for i in barrels:
-    # print(barrels[i])
-    if 80 < barrels[i]<= 90:
-        numbers81_90.append(barrels[i])
-new_barrels.append(numbers81_90)
+    for i in barrels:
+        # print(barrels[i])
+        if 30 < barrels[i] <= 40:
+            numbers31_40.append(barrels[i])
+    new_barrels.append(numbers31_40)
 
+    for i in barrels:
+        # print(barrels[i])
+        if 40 < barrels[i] <= 50:
+            numbers41_50.append(barrels[i])
+    new_barrels.append(numbers41_50)
 
-for n in numbers1_10:
-    print(n)
-print(numbers1_10)
-print(numbers11_20)
-print(numbers21_30)
-print(numbers31_40)
-print(numbers41_50)
-print(numbers51_60)
-print(numbers61_70)
-print(numbers71_80)
-print(numbers81_90)
+    for i in barrels:
+        # print(barrels[i])
+        if 50 < barrels[i] <= 60:
+            numbers51_60.append(barrels[i])
+    new_barrels.append(numbers51_60)
 
-x=[i for i in range(9)]
-random.shuffle(x)
-print(x)
-x1=x[:5]
-print(x1[0])
-random.shuffle(x)
-print(x)
-x2=x[:5]
-random.shuffle(x)
-print(x)
-x3=x[:5]
-print(x1)
-line1=[]
-line2=[]
-line3=[]
-print(new_barrels)
-random.shuffle(new_barrels)
-print(new_barrels)
-print()
-for i in range(5):
-    line1.append(new_barrels[x1[i]][0])
-for i in range(5):
-    line2.append(new_barrels[x2[i]][1])
-for i in range(5):
-    line3.append(new_barrels[x3[i]][2])
+    for i in barrels:
+        # print(barrels[i])
+        if 60 < barrels[i] <= 70:
+            numbers61_70.append(barrels[i])
+    new_barrels.append(numbers61_70)
 
-print(line1)
-print(line2)
-print(line3)
+    for i in barrels:
+        # print(barrels[i])
+        if 70 < barrels[i] <= 80:
+            numbers71_80.append(barrels[i])
+    new_barrels.append(numbers71_80)
 
+    for i in barrels:
+        # print(barrels[i])
+        if 80 < barrels[i] <= 90:
+            numbers81_90.append(barrels[i])
+    new_barrels.append(numbers81_90)
 
+    # for n in numbers1_10:
+    #     print(n)
+    # print(numbers1_10)
+    # print(numbers11_20)
+    # print(numbers21_30)
+    # print(numbers31_40)
+    # print(numbers41_50)
+    # print(numbers51_60)
+    # print(numbers61_70)
+    # print(numbers71_80)
+    # print(numbers81_90)
 
+    x = [i for i in range(9)]
+    random.shuffle(x)
+    # print(x)
+    x1 = x[:5]
+    # print(x1[0])
+    random.shuffle(x)
+    # print(x)
+    x2 = x[:5]
+    random.shuffle(x)
+    # print(x)
+    x3 = x[:5]
+    # print(x1)
+    line1 = []
+    line2 = []
+    line3 = []
+    # print(new_barrels)
+    random.shuffle(new_barrels)
+    # print(new_barrels)
+    # print()
+    for i in range(5):
+        line1.append(new_barrels[x1[i]][0])
+    for i in range(5):
+        line2.append(new_barrels[x2[i]][1])
+    for i in range(5):
+        line3.append(new_barrels[x3[i]][2])
+    #
+    # print(line1)
+    # print(line2)
+    # print(line3)
 
+    line1 = sorted(line1)
+    line2 = sorted(line2)
+    line3 = sorted(line3)
 
+    # print()
+    # print(line1)
+    # print(line2)
+    # print(line3)
+
+    def end_line(line1):
+        line1end = ['_', '_', '_', '_', '_', '_', '_', '_', '_', ]
+        for i in line1:
+            if 0 < i <= 10:
+                line1end[0] = i
+
+            if 10 < i <= 20:
+                line1end[1] = i
+
+            if 20 < i <= 30:
+                line1end[2] = i
+
+            if 30 < i <= 40:
+                line1end[3] = i
+
+            if 40 < i <= 50:
+                line1end[4] = i
+
+            if 50 < i <= 60:
+                line1end[5] = i
+
+            if 60 < i <= 70:
+                line1end[6] = i
+
+            if 70 < i <= 80:
+                line1end[7] = i
+
+            if 80 < i <= 90:
+                line1end[8] = i
+
+        return line1end
+
+    # print()
+
+    end_barrels = [[], [], []]
+    end_barrels[0] = end_line(line1)
+    end_barrels[1] = end_line(line2)
+    end_barrels[2] = end_line(line3)
+
+    # print(end_barrels)
+    return end_barrels
+
+# new_card()
