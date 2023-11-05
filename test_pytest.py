f2f810e4cd9290162607b863a5469832843d1519
Artem Khimin <a.khimin@yandex.ru>
2023-11-06 02:16:05 +0300

    python -m pytest --cov-report html --cov .

--- test_pytest.py
+++ test_pytest.py
@@ -1,15 +1,24 @@
 import random_card
 import main
 
+class TestGameLoto:
+    def setup(self):
+        pass
 
-def test_barrels_mix():
-    barrels = random_card.barrels_mix()
-    assert len(barrels) == 90
+    def teardown(self):
+        pass
+
+    def test_barrels_mix(self):
+        barrels = random_card.barrels_mix()
+        assert len(barrels) == 90
 
 
-def test_field():
-    rtn = main.field()
-    assert rtn == None
+    def test_end_line(self):
+        end_barrels = random_card.new_card()
+        assert type(end_barrels) == list
 
 
-
+# def test_steep():
+#     rtn = main.steep()
+#     print(rtn)
+#     assert rtn == None
